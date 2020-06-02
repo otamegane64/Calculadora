eb76d46fdcdd9decd70431377cf58d72b9545793
GeozakiGr <57080326+GeozakiGr@users.noreply.github.com>
2020-06-01 20:03:03 -0600

    operacion de divicion

---
+++ calculadora-init-master/src/main/java/wsCalculadora/iscrquinter/hotmail/com/Calculadora/Calculadora.java
@@ -0,0 +1,27 @@
+package wsCalculadora.iscrquinter.hotmail.com.Calculadora;
+
+public class Calculadora {
+
+	public double suma(double x, double y) {
+		return x + y;
+	}
+
+	public double resta(double x, double y) {
+		return x-y;
+	}
+
+	public double multiplica(double x, double y) {
+		return x * y;
+	}
+	public double divide(double x, double y){
+	try {
+		return x/y;
+	
+	} catch (Exception e) {
+		//TODO: handle exception
+	}
+		return 0;
+		
+	}
+    
+}
